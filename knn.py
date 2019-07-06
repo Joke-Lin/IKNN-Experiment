ab93bb3a21bedf5666f1156b051afdb23635a0ef
Joke-Lin <2311349863@qq.com>
2019-07-06 18:02:30 +0800

    add knn definition

---
+++ knn.py
@@ -0,0 +1,9 @@
+import preprocess
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mp
+import quadTree
+import preprocess
+
+def knn(point: list, dataSet: np.ndarray, tree: quadTree.QuadTree) -> list:
+    pass
